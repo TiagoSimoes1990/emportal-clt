da5b8bc162b744085bf391442e8b3c0585ea0cde
Tiago Simões <tiagojfsimoes@gmail.com>
2024-04-18 11:53:05 +0100

    refact: removed unused imports and add comments to improve readability

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import './App.css';
 import * as React from 'react';
+// External components
 import UserCard from './components/user-card';
 import ModalDialog from './scenes/user-modal/modal-dialog'
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemButton from '@mui/material/ListItemButton';
-
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import CloseIcon from '@mui/icons-material/Close';
-import Slide from '@mui/material/Slide';
-
 import Topbar from './scenes/topbar/topbar';
-import ModalSidebar from './scenes/user-modal-global/modal-sidebar';
-
-// ProSide Bar plugin
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-
 // Data import
 import { userData } from './data/mockdata';
 import { Box } from '@mui/material';
+// Styles
+import './App.css';
 
 // -----------------------
 // Main component
