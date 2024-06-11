7e5954e55c924d060a66dd40ecd2ca906ec14825
Tiago Simões <tiagojfsimoes@gmail.com>
2024-06-11 23:22:29 +0100

    refact: Create a style inside of react component instead of import the style.css file

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -3,17 +3,23 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import '../styles/user-card.css';
+
+const userImg = {
+  borderRadius: "50%",
+  border: "7px solid #fff",
+  height: "120px",
+  width: "120px"
+};
 
 function UserCard(props) {
   return (
-    <Card className="user-card" sx={{minWidth: 250, maxWidth: 275, m:1 }}>
+    <Card style={userImg} sx={{minWidth: 250, maxWidth: 275, m:1 }}>
       <CardActionArea
         onClick={ () => {
           props.onCardClick (props.id)
           }
         }>
-        <img className="circle-img" src={props.userPhoto} />
+        <img className="circle-img" src={props.userPhoto} alt='user photo'/>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {props.firstName + " " + props.lastName}
