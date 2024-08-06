b0b6350cfcff2677240015ce2998c78b915c78cf
Tiago Simões <tiagojfsimoes@gmail.com>
2024-08-06 12:54:33 +0100

    refact(user-card): Replace the usage of <img> tag with <Avatar> react component

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -2,15 +2,9 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import { CardActionArea, Stack } from '@mui/material';
 import '../styles/user-card.css';
-
-const userImg = {
-  borderRadius: "50%",
-  border: "7px solid #fff",
-  height: "120px",
-  width: "120px"
-};
 
 function UserCard(props) {
   return (
@@ -21,21 +15,31 @@ function UserCard(props) {
           props.onCardClick (props.id)
           }
         }>
-        <img className="circle-img" src={props.userPhoto} alt='user photo'/>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.firstName + " " + props.lastName}
-          </Typography>
-          <Typography gutterBottom variant="h7" component="div">
-            {props.position}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {props.email}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {props.phoneNumber}
-          </Typography>
-        </CardContent>
+        <Stack 
+          direction="column" 
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}>
+          <Avatar
+            alt='user photo'
+            src={props.userPhoto}
+            sx={{ width: 120, height: 120 }}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {props.firstName + " " + props.lastName}
+            </Typography>
+            <Typography gutterBottom variant="h7" component="div">
+              {props.position}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {props.email}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {props.phoneNumber}
+            </Typography>
+          </CardContent>
+        </Stack>
       </CardActionArea>
     </Card>
   );
